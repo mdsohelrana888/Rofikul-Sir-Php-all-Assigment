42e27aa36888c1113d9c9aafdb0a02ccb30a2d3d
Coder0Labib <webcoderlabib@gmail.com>
2022-04-07 16:18:20 +0600

    07 April 2022

--- Assigment class 04/index.php
+++ Assigment class 04/index.php
@@ -22,7 +22,13 @@ echo  "<br>";
     }
     echo  "<br>";
     echo  "<br>";
-    // // Question 03 Assignment : 
+    Question 03: 
+
+// Factorial - I think everyone knows the word. Factorial of 4 is given below:
+// 4! = 4*3*2*1 = 24
+// 6! = 6*5*4*3*2*1 = 720  
+// So let’s calculate the factorial of 7.
+
     echo  "Question 03 Assignment";
     $num = 7;  
     $factorial = 1;  
@@ -35,7 +41,11 @@ echo  "<br>";
     echo  "<br>";
     echo  "<br>";
    
-    // // Question 04 Assignment : 
+// Question 04 Assignment : 
+// Question 04: 
+// Print out from 100 - 1 using a while loop. 
+// You must use decrement operator
+
     echo  "Question 04 Assignment";
     echo  "<br>";
     $x = 100;

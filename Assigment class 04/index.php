97f444c27f77800c7e4a8e9ec107b6077ae8a418
Coder0Labib <webcoderlabib@gmail.com>
2022-04-07 16:15:51 +0600

    07 April 2022

--- Assigment class 04/index.php
+++ Assigment class 04/index.php
@@ -5,7 +5,11 @@
 
 echo  "<br>";
 
-// // Question 02 Assignment : 
+  // Question 02 Assignment : Question 02: 
+	//If we print 1-100, our output will be 100 times. But this time you have print out only the odd (বিজোড়) number within 1-100
+  // You have to rotate from 1 to 100.
+  // Your output message should like that, “3 is a odd number”
+  // Not need to print even number
 echo  "Question 02 Assignment";
 echo  "<br>";
    $end=100;

dd9da6bae60aecf489debd404f7d04764fa8b675
Coder0Labib <webcoderlabib@gmail.com>
2022-04-16 13:45:58 +0600

    16 April 2022

--- Assigment class 06/assigment_class_06.php
+++ Assigment class 06/assigment_class_06.php
@@ -1,86 +1,56 @@
 <?php
+// 	Question 01:====================================================================================
+//     You have purchased some items from a supershop. Purchase product array is given below-
+// $purchased_products = array(
+//             array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+//             array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+//             array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
+//     );
+// How much product quantity you have purchased (total purchased qty = 67)
+// Calculate the total amount of all purchased items (total amount = 2650 tk).
 
-// Question 01: ================================================================================================
-// We have an array [12,34,2,6,78]. What kind of array is this? Find all prime numbers from this array.
-// Step 1: First let us find the factors of the given number( factors are the number that completely divides the given number)
-// Step 2: Then check the total number of factors of that number
-// Step 3: Hence, If the total number of factors is more than two, it is not a prime number but a composite number. Because When a number is divisible by only one and itself, then it is a prime number.
-// Example Website Jahan takha Code ta bujta parci Link:https://www.alphacodingskills.com/php/pages/php-program-to-check-prime-number.php
+	$purchased_products = array(
+        array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+        array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+        array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
 
-$Number =[12,34,2,6,78,7];
-foreach ($Number as $key => $value) {
-   $Number =$value ;
-   $n = 0;
-
-for($i = 2; $i < $Number; $i++) {
- if($Number % $i == 0){
-   $n++;
-   break;
- }
-}
-if ($n == 0){
-   echo $Number." is a prime number.";
-   echo "<br>";
- } else {
-     continue;
-     echo "<br>";
-   // echo $Number." is not a prime number.";
- }
-}
+);
+$results = array_sum(array_column($purchased_products, 'qty'));
+$total_price = array_sum(array_column($purchased_products, 'total_price'));
+echo "Total purchased qty=$results";
 echo "<br>";
-echo "<br>";
-// Question 02: ==============================================================
-// Let’s think about a use case, Daraz admin’s have some products for uploads on their e-commerce site. So make an array of products which contains 5 products.
-// Go to daraz website and pick any 5 products and make your array.
-// Must mention Which type of array you have made.
+echo "total amount =$Total_price";
 
-//Multidimensional Arrays
-$products_List = [
 
-   ['Product-Name' => 'Shirt', 'Price'   => 320, 'size'  => 'M' , 'Color'         => 'Blue' ],
-   ['Product-Name' => 'Pant', 'Price'    => 560, 'size'  => "36 Inch" , 'Color'   => 'Black' ],
-   ['Product-Name' => 'Sari', 'Price'    => 3000, 'size' => 'Free Size' , 'Color' => 'Gray' ],
-   ['Product-Name' => 'T-shirt', 'Price' => 160, 'size'  => 'M' , 'Color'         => 'Skyblue' ],
-   ['Product-Name' => 'Cap', 'Price'     => 80, 'size'   => 'free Size' , 'Color' => 'White' ]
- 
- ]; 
- 
- foreach ( $products_List as $key => $SingleProduct) {
-   echo "<hr>";  
-   echo "$key:";
-     foreach ( $SingleProduct as $key1 => $value) {
-         echo $key1.":". $value;
-         echo "<br>";
-     }
-     
- }
-   
-echo"<br>";
-echo "<br>";
-// ========================================================================
-// Question 03: 
-// This is an array array(0,10,80,67,60,89,91,56,45,30,95,83,99) 
-// Find the maximum value from this array.
-// Must use a foreach loop
-$a = [0,10,80,67,60,89,91,56,45,30,95,83,99];
-$b = 0;
-foreach ($a as $key=>$value) {
-    if ($value > $b) {
-        $b = $value;
-    }
-}
-echo "Maximum value:$b";
-echo "<br>";
-echo "<br>";
+// Question 02  =======================================================================================       : 
+// You have two array of emails, these are given below-
+// $first_email_array  = ["abc@gmail.com", "xyz@gmail.com", "demo@gmail.com"]; 
+// $second_email_array = ["efg@gmail.com", "xyz@gmail.com", "dummy@gmail.com"];
+// You have to make a single array from this two arrays
+// And don’t keep any value in multiple time
+$first_email_array     = ["abc@gmail.com", "xyz@gmail.com", "demo@gmail.com"]; 
+$second_email_array    = ["efg@gmail.com", "xyz@gmail.com", "dummy@gmail.com"];
+$results               = array_unique(array_merge($first_email_array, $second_email_array));
+ echo "<pre>";
+ print_r ($results);
+ echo "</pre";
+ echo "<br>";
+ $purchased_products = array(
+    array('name' => 'banana',  'qty' => 10, 'total_price' => 550),
+    array('name' => 'apple',     'qty' => 50,  'total_price' => 1500),
+    array('name' => 'orange',  'qty' => 7,    'total_price' => 600),
 
-// Question 04: ===========================================================================
-//   Let’s traverse this array(0,10,80,67,60,89,91,56,45,30,95,83,99) and print 0 index then skip 1 index again print 2 skip 3….
+);
 
-$array = [0,10,80,67,60,89,91,56,45,30,95,83,99]; 
-foreach ($array  as $key => $value) {
-        if ($key % 2) {
-           continue ;
-}
- echo  "$value<br>";
-}
+ echo "<br>";
+ echo "<br>";
+
+//  Question 03: =========================================================================
+// 	$citylist  = array( "Black Canyon City", "Chandler", "Flagstaff", "Gilbert", "Glendale", "Globe", "Mesa", "Miami", "Phoenix", "Peoria", "Prescott", "Scottsdale", "Sun City", "Surprise", "Tempe", "Tucson", "Wickenburg" );
+// Split this given array into chunks 4
+
+ $citylist = array( "Black Canyon City", "Chandler", "Flagstaff", "Gilbert", "Glendale", "Globe", "Mesa", "Miami", "Phoenix", "Peoria", "Prescott", "Scottsdale", "Sun City", "Surprise", "Tempe", "Tucson", "Wickenburg" );
+  echo "<pre>";
+ print_r(array_chunk($citylist,4));
+
 ?>

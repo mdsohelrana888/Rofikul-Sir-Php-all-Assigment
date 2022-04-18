b6cebe5c879d195c5d5a2e1d3da46454f029607e
Coder0Labib <webcoderlabib@gmail.com>
2022-04-18 14:38:19 +0600

    18 April 2022

---
+++ Assigment class 07/test.php
@@ -0,0 +1,11 @@
+
+<?php
+$input = ['name' => 'banana',  'qty' => 10, 'total_price' => 550 ];
+$input = ['name' => 'banana',  'qty' => 10, 'total_price' => 550 ];
+$input = ['name' => 'banana',  'qty' => 10, 'total_price' => 550 ];
+$input = ['name' => 'banana',  'qty' => 10, 'total_price' => 550 ];
+ foreach ($input as $key => $value) {
+         echo $input['name'];
+ }
+
+?>

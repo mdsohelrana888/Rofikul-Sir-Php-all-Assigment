d498babb8bbc1ccb26d99704d61ae5dc6d7f75f7
Coder0Labib <webcoderlabib@gmail.com>
2022-05-08 20:06:12 +0600

    30 April 2022

--- Assigment class 09/assigment_class_09.php
+++ Assigment class 09/assigment_class_09.php
@@ -1,113 +1,309 @@
-<?php
+<!doctype html>
+<html lang='en'>
 
-// 1.Create a function that will return children, teenagers, young people, old people from the age=============================================
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <title> Developer Nurul Islam</title>
+    <style>
+        :root {
+	/* COLORS */
+	--white: #e9e9e9;
+	--gray: #333;
+	--blue: #0367a6;
+	--lightblue: #008997;
 
-function people_age_Group($age) {
-      if($age <=0){
-              return 'Not valid Age Number';
-      }elseif ($age <=12) {
-        return 'You are Kids';
-      }elseif ($age <=20) {
-        return 'You are Teenagers';
-      }elseif ($age <=39) {
-        return 'You are Adults';
-      }elseif ($age <=69) {
-        return 'Middle Age Adult ';
-      }
-      elseif($age<=100) {
-        return 'You are old man ';
-      }else{
-        return "Not valid Age Number";
-      }
+	/* RADII */
+	--button-radius: 0.7rem;
 
+	/* SIZES */
+	--max-width: 758px;
+	--max-height: 420px;
 
-};
-echo people_age_Group(-02);
-
-echo "<br>";
-echo "<br>";
-
-// 2.Create a function that will return the area of a rectangle, square, triangle
-// Rectangle function
-function rect_area($length, $width) //function has not declared
-{
-    $area = $length * $width;
-    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
-}
-echo rect_area(23,10);
-echo "<br>";
-echo "<br>";
-// square
-function areaSquare($side)
-{
-	$area = $side * $side;
-	return 'Area Of square'. $area;
+	font-size: 16px;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+		Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 
-echo areaSquare(10);
-echo "<br>";
-echo "<br>";
-// triangle
-function tri_area($length, $width) //function has not declared
-{
-    $area = ($length * $width)/2;
-    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
-}
-echo tri_area(23,10);
-
-echo "<br>";
-echo "<br>";
-
-// 3.GPA function for result publishing===============================================
-function gpa_calculate($SubjectA,$SubjectB,$SubjectC,$SubjectD){
-  $total = $SubjectA + $SubjectB+ $SubjectC + $SubjectD;
-  $gpa = $total / 4;
-
-  if ($gpa >=80){
-      echo " You Got A+";
-  }else if ($gpa >=70 && $gpa <=79){
-      echo " You Got A";
-  }else if($gpa >=60 && $gpa <=69){
-      echo " You Got A-";
-  }else if ($gpa >=50 && $gpa <=59){
-      echo " You Got B";
-  }else if ($gpa >=40 && $gpa <=49){
-      echo " You Got C";
-  }else if ($gpa >= 01 && $gpa <=39){
-      echo " You Got F";
-};
-return $gpa ;
-}
-echo  gpa_calculate(20,60,100,90);
-
-
-echo "<br>";
-echo "<br>";
-// 4.Create an age calculator function
-function getAge($dob,$condate){ 
-  $birthdate = new DateTime(date("Y-m-d",  strtotime(implode('-', array_reverse(explode('/', $dob))))));
-  $today= new DateTime(date("Y-m-d",  strtotime(implode('-', array_reverse(explode('/', $condate))))));           
-  $age = $birthdate->diff($today)->y;
-
-  return $age;
+body {
+	align-items: center;
+	background-color: var(--white);
+	background: url("https://res.cloudinary.com/dci1eujqw/image/upload/v1616769558/Codepen/waldemar-brandt-aThdSdgx0YM-unsplash_cnq4sb.jpg");
+	background-attachment: fixed;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	display: grid;
+	height: 100vh;
+	place-items: center;
 }
 
-echo getAge('06/06/1996','07/02/2011');
-echo "<br>";
-echo "<br>";
-// 5.Create a BMI function for health
-function BmiClaculator($wight, $height){
-  $height = $height/100;
+.form__title {
+	font-weight: 300;
+	margin: 0;
+	margin-bottom: 1.25rem;
+}
 
-  $bmi_result = $wight / ($height * $height);
-  $bmi_result = number_format($bmi_result, 2);
-  return "Your BMI = {$bmi_result}" ;
+.link {
+	color: var(--gray);
+	font-size: 0.9rem;
+	margin: 1.5rem 0;
+	text-decoration: none;
+}
 
+.container {
+	background-color: var(--white);
+	border-radius: var(--button-radius);
+	box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25),
+		0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
+	height: var(--max-height);
+	max-width: var(--max-width);
+	overflow: hidden;
+	position: relative;
+	width: 100%;
+}
 
-};
-// first argument is weight (kg) and 2nd argument is height( cm )
-echo BmiClaculator( 56, 57);
+.container__form {
+	height: 100%;
+	position: absolute;
+	top: 0;
+	transition: all 0.6s ease-in-out;
+}
 
+.container--signin {
+	left: 0;
+	width: 50%;
+	z-index: 2;
+}
 
+.container.right-panel-active .container--signin {
+	transform: translateX(100%);
+}
 
-?>
+.container--signup {
+	left: 0;
+	opacity: 0;
+	width: 50%;
+	z-index: 1;
+}
+
+.container.right-panel-active .container--signup {
+	animation: show 0.6s;
+	opacity: 1;
+	transform: translateX(100%);
+	z-index: 5;
+}
+
+.container__overlay {
+	height: 100%;
+	left: 50%;
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	transition: transform 0.6s ease-in-out;
+	width: 50%;
+	z-index: 100;
+}
+
+.container.right-panel-active .container__overlay {
+	transform: translateX(-100%);
+}
+
+.overlay {
+	background-color: var(--lightblue);
+	background: url("https://res.cloudinary.com/dci1eujqw/image/upload/v1616769558/Codepen/waldemar-brandt-aThdSdgx0YM-unsplash_cnq4sb.jpg");
+	background-attachment: fixed;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	height: 100%;
+	left: -100%;
+	position: relative;
+	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+	width: 200%;
+}
+
+.container.right-panel-active .overlay {
+	transform: translateX(50%);
+}
+
+.overlay__panel {
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: center;
+	position: absolute;
+	text-align: center;
+	top: 0;
+	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+	width: 50%;
+}
+
+.overlay--left {
+	transform: translateX(-20%);
+}
+
+.container.right-panel-active .overlay--left {
+	transform: translateX(0);
+}
+
+.overlay--right {
+	right: 0;
+	transform: translateX(0);
+}
+
+.container.right-panel-active .overlay--right {
+	transform: translateX(20%);
+}
+
+.btn, .btm {
+	background-color: var(--blue);
+	background-image: linear-gradient(90deg, var(--blue) 0%, var(--lightblue) 74%);
+	border-radius: 20px;
+	border: 1px solid var(--blue);
+	color: var(--white);
+	cursor: pointer;
+	font-size: 0.8rem;
+	font-weight: bold;
+	letter-spacing: 0.1rem;
+	padding: 0.9rem 4rem;
+	text-transform: uppercase;
+	transition: transform 80ms ease-in;
+}
+
+.form > .btn {
+	margin-top: 1.5rem;
+}
+
+.btn:active {
+	transform: scale(0.95);
+}
+
+.btn:focus {
+	outline: none;
+}
+
+.form {
+	background-color: var(--white);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 3rem;
+	height: 100%;
+	text-align: center;
+}
+
+.input {
+	background-color: #fff;
+	border: none;
+	padding: 0.9rem 0.9rem;
+	margin: 0.5rem 0;
+	width: 100%;
+}
+
+@keyframes show {
+	0%,
+	49.99% {
+		opacity: 0;
+		z-index: 1;
+	}
+
+	50%,
+	100% {
+		opacity: 1;
+		z-index: 5;
+	}
+}
+.form-div {
+            width: 500px;
+            margin: 0 auto;
+}
+    </style>
+</head>
+
+<body>
+	<?php
+	
+	if (isset ($_POST["user_btn"])) {
+	
+		$name = $_POST['name'];
+		$user_name = $_POST['user_name'];
+		$email = $_POST['email'];
+
+	if ($name == "" || $user_name == "" || $email == "") {
+	
+		echo "You have to filed all";
+	
+	}else{
+		echo "thank";
+	}
+
+		}
+	
+	?>
+    
+    <div class="container right-panel-active">
+        <!-- Sign Up -->
+        <div class="container__form container--signup">
+            <form action="" class="form" id="form1" method="POST" >
+                <h2 class="form__title">Sign Up</h2>
+                <input type="text" placeholder="Your Name" name="name" class="input" />
+                <input type="text" placeholder="User" name="user_name" class="input" />
+                <input type="email" placeholder="Email" name="email" class="input" />
+                <input type="password" placeholder="Password" class="input" />
+                <!-- <button class="btn">Sign Up</button> -->
+                <input type="submit" class="btm" name="user_btn" value="Sign Up" >
+            </form>
+        </div>
+
+        <!-- Sign In -->
+        <div class="container__form container--signin">
+            <form action="#" class="form" id="form2" method="post">
+                <h2 class="form__title">Sign In</h2>
+                <input type="email" placeholder="Email" class="input" />
+                <input type="password" placeholder="Password" class="input" />
+                <a href="#" class="link">Forgot your password?</a>
+				<input type="submit" class="btm" value="Sign In" >
+            </form>
+        </div>
+
+        <!-- Overlay -->
+        <div class="container__overlay">
+            <div class="overlay">
+                <div class="overlay__panel overlay--left">
+                    <button class="btn" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay__panel overlay--right">
+                    <button class="btn" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <script>
+        const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
+    </script> -->
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p' crossorigin='anonymous'></script>
+    IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js' integrity='sha384-QJHtvGhmr9XOIpI6YVut+2QOK9T
++ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13' crossorigin='anonymous'></script>
+</body>
+
+</html>
